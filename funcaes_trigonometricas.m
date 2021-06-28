7845ae2df55f2feb50c565d726c47f7e118333fe
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-06-28 03:20:18 -0300

    primeiros Scripts MATLAB

---
+++ funcaes_trigonometricas.m
@@ -0,0 +1,35 @@
+clc;
+clear all;
+close all;
+
+a = 45;
+b = 90;
+c = 180;
+
+%% Seno e cosseno em Radianos
+
+% Transformando ângulos de Graus para Radiano
+
+ar = (a * pi) / 180;
+br = (b * pi) / 180;
+cr = (c * pi) / 180;
+
+sin(ar)
+sin(br)
+sin(cr)
+
+cos(ar)
+cos(br)
+cos(cr)
+
+%% Seno dos ângulos em Graus
+
+sind(a)
+sind(b)
+sind(c)
+
+% Cosseno dos ângulos em Graus
+
+cosd(a)
+cosd(b)
+cosd(c)
---
+++ operacaes_matematicas.m
@@ -0,0 +1,52 @@
+clc;
+clear all;
+close all;
+
+%% Operações Matemáticas
+
+a = 4;
+b = 5;
+
+% Soma
+
+c = a + b;
+c
+
+% Subtração
+
+d = a - b;
+d
+
+% Multiplicação
+
+e = a * b;
+e
+
+% Divisão
+
+f = a / b;
+f
+f = a \ b;
+f
+
+% Raiz Quadrada
+
+g = sqrt(a);
+g
+
+% Potênciação
+
+h = a^2;
+h
+
+% Exponencial
+
+i = exp(a);
+i
+
+% Logarítimos
+
+j = log10(20);
+j
+
+
