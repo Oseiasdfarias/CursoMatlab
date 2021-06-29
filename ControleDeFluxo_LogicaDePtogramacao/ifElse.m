e7e238e665ba614116c0c3347136d49c018e74c0
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-06-28 22:24:14 -0300

    atualização, pasta logicaDeProgramação

--- ControleDeFluxo_LogicaDePtogramacao/ifElse.m
+++ ControleDeFluxo_LogicaDePtogramacao/ifElse.m
@@ -2,11 +2,12 @@ clc;
 clearvars;
 close all;
 
-% Lógica IF ELSE
+% Lógica IF ELSEIF ELSE 
 
-a = input ('Digite um valor: ');
-b = input ('Digite outro valor: ');
+a = input('Digite um valor: ');
+b = input('Digite outro valor: ');
 
+% && é o operador booleano END/E
 if a >= 0 && b >= 0
     fprintf('\n%d e %d são valores positivos!\n', a, b);
 elseif a >= 0 && b < 0
@@ -17,3 +18,29 @@ else
     fprintf('\n%d e %d São valores Negativos\n', a, b);
 end
 
+%% 
+
+c = input('Digite um valor: ');
+d = input('Digite outro valor: ');
+
+%  || e o operador booleano OR\OU
+if c >= 0 || d >= 0
+    fprintf('\nUm dos valores é positivo\n');
+else
+    fprintf('\nOs valores são todos Negativos\n');
+end
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
